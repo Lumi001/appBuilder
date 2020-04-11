8cf33b406325c24219b02cc61fc485f0acecac5d
Pelumi <akinola001pelumi@gmail.com>
2020-04-11 19:51:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/form/Form'
+
+function App() {
+  return (
+   <Form/>
+  
+  );
+}
+
+export default App;
---
+++ src/components/form/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './form.css'
+function form() {
+    return (
+        <div className="form">
+      <h1>BUTTON TEXT</h1>
+        <p>Enter text</p>
+        <input type="text" placeholder="Click me"/>
+    </div>
+    )
+}
+
+export default form
